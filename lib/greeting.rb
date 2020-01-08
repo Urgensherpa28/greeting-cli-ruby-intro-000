2c1c8f71000efcf32e4ec6106a27525f2c290144
Urgen Sherpa <urgen.nyc@gmail.com>
2020-01-08 20:08:36 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,6 @@
 # code the #greeting method here!
-def greeting
-  puts "Hello, ! It's nice to meet you."
+def greeting(name)
+  puts "Hello, #{name}! It's nice to meet you."
 end
 
 return greeting
